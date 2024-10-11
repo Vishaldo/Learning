1e9870c510cb87afea11b2c5f9aac815b2fda240
Vishaldo <140370601+Vishaldo@users.noreply.github.com>
2024-10-11 12:08:32 +0530

    Program.java file  added

--- Program1.java
+++ Program1.java
@@ -1,6 +1,21 @@
+
+import java.util.Scanner;
+
 class Program1{
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a : ");
+        int a = scan.nextInt();
+        System.out.print("Enter b : ");
+        int b = scan.nextInt();
+        System.out.println("Enter c : ");
+        int c = scan.nextInt();
+        scan.nextInt();
+        int d = a*b*c;
+        System.out.println(d);
+        int e = a+b+c+d;
+        System.out.println(e);
+
 }
 
 }
