907a8aab2a1e3102101c02257bcd449fbea5a267
Vishaldo <140370601+Vishaldo@users.noreply.github.com>
2024-10-11 11:45:46 +0530

    Program.java file  added

--- Program.java
+++ Program.java
@@ -4,13 +4,13 @@ class Program {
     public static void main(String args[])
         {
             Scanner var1 = new Scanner(System.in);
-            Scanner var2 = new Scanner(System.in);
             System.out.print("Enter your Name: ");
             String name = var1.nextLine();
             System.out.print("Age : ");
             int age = var1.nextInt();
+            var1.nextLine();
             System.out.print("Address: ");
-            String Address = var2.nextLine();
+            String Address = var1.nextLine();
             System.out.println("Name : " + name);
             System.out.println("Age : " + age);
             System.out.println("Address : " + Address);
